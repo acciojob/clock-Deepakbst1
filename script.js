4e8251ac022e20df01d6e3c2c2eca441f35f8bf5
Deepak Kumar <91386273+Deepakbst1@users.noreply.github.com>
2023-05-25 22:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function updateTimer() {
+      var now = new Date();
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds();
+
+      // Add leading zeros if necessary
+      hours = hours < 10 ? '0' + hours : hours;
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      var timeString = hours + ':' + minutes + ':' + seconds;
+      document.getElementById('timer').textContent = timeString;
+
+      // Update the timer every second
+      setTimeout(updateTimer, 1000);
+    }
+
+    // Start the timer initially
+    updateTimer();
